87e6edf809e79e543226bc95021af5306466f288
TianShuang Ke <QinYUN575@Gmail.com>
2024-05-29 23:43:24 +0800

    new demo

--- remote_voice_enable.py
+++ remote_voice_enable.py
@@ -2,12 +2,13 @@ from bluepy.btle import Peripheral, DefaultDelegate, UUID
 import time
 
 # 已知的蓝牙设备地址
-# device_address = "0C:F5:33:16:64:7B"  # 替换为实际设备地址
-device_address = "0c:f5:33:41:1a:da"  # 替换为实际设备地址
+device_address = "0C:F5:33:16:64:7B"  # 替换为实际设备地址
+# device_address = "0c:f5:33:41:1a:da"
 service_uuid = UUID("00001812-0000-1000-8000-00805f9b34fb")
 
-
 # 代理类，用于处理通知
+
+
 class NotificationDelegate(DefaultDelegate):
     def __init__(self):
         DefaultDelegate.__init__(self)
@@ -24,42 +25,50 @@ device = Peripheral(device_address, "public")
 
 try:
     # 获取特定服务
-    service = device.getServiceByUUID(service_uuid)
-    characteristics = service.getCharacteristics()
+    hid_service = device.getServiceByUUID(service_uuid)
+    hid_characteristics_list = hid_service.getCharacteristics()
 
     # 检查服务中有足够的特征
-    if len(characteristics) >= 12:
+    if len(hid_characteristics_list) >= 12:
         try:
             device.pair()
             print("Paired successfully")
         except Exception as e:
             device.unpair()
-            print(f"Pairing failed: {e}")
-            
-            
-        target_char_write = characteristics[11]
-        print(f"Writing to characteristic: {target_char_write.uuid}")
+            # print(f"Pairing failed: {e}")
+
+        uuid_voice_enable = hid_characteristics_list[11]
+        print(f"Writing to characteristic: {uuid_voice_enable.uuid}")
 
         # # 写入值 '01'
-        target_char_write.write(b'\x01')
+        uuid_voice_enable.write(b'\x01')
         print("Write successful")
-        target_char_notify = characteristics[10]
-        print(f"Enabling notifications for characteristic: {target_char_notify.uuid}")
+
+        uuid_voice_notify = hid_characteristics_list[10]
+        print(
+            f"Enabling notifications for characteristic: {uuid_voice_notify.uuid}")
 
         # 设置代理并开始监听通知
         delegate = NotificationDelegate()
         with device.withDelegate(delegate):
             # 启用通知
-            target_char_notify.write(b'\x01\x00')  # 启用通知的典型值
+            uuid_voice_notify.write(b'\x01\x00')  # 启用通知的典型值
             count = 0
             # 持续监听
             while True:
-                if count >= 10:
-                    target_char_notify.write(b'\x00\x00')  # 启用通知的典型值
+                if count >= 5:
+                    # uuid_voice_notify.write(b'\x00\x00')  # 启用通知的典型值
+                    print("---->Disable RemoteVoice Voice Data")
+                    uuid_voice_enable.write(b'\x00')
+                    # device.unpair()
+                    # break
+                print("sleep")
+                time.sleep(1)
                 if device.waitForNotifications(3.0):
-                    continue
-                print("Waiting for notifications...")
+                    print("Waiting for notifications...")
+                    # continue
                 count += 1
+
     else:
         print(f"Service {service_uuid} does not have 11 characteristics")
 finally:
